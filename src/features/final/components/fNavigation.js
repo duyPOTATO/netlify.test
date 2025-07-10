732d45a1dcc14c8242d4993de253bfa005a26583
duPO <dupo.duypotato@gmail.com>
2025-07-10 22:36:25 +0700

    sửa href

--- src/features/final/components/fNavigation.js
+++ src/features/final/components/fNavigation.js
@@ -17,9 +17,9 @@ function FNavigation() {
                 <div className="nav-right">
                     <ul>
                         <li><a className="nav-text nav-home" href="#">HOME</a></li>
-                        <li><a className="nav-text nav-works" href="#page-works">MY WORK</a></li>
-                        <li><a className="nav-text nav-about" href="#page-about">ABOUT</a></li>
-                        <li><a className="nav-text nav-contact" href="#page-contact">CONTACT</a></li>
+                        <li><a className="nav-text nav-works" href="#">MY WORK</a></li>
+                        <li><a className="nav-text nav-about" href="#">ABOUT</a></li>
+                        <li><a className="nav-text nav-contact" href="#">CONTACT</a></li>
                     </ul>
                     <div className='toggle-button' href="#" onClick={handleClick}>
                         <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-menu-2" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
@@ -34,9 +34,9 @@ function FNavigation() {
             <div className={`md:hidden nav-menu-mobile bg-white ${active ? 'active' : ''}`}>
                 <ul>
                     <li><a className="nav-menu-text nav-menu-home" href="#">HOME</a></li>
-                    <li><a className="nav-menu-text nav-menu-works" href="#page-works">MY WORK</a></li>
-                    <li><a className="nav-menu-text nav-menu-about" href="#page-about">ABOUT</a></li>
-                    <li><a className="nav-menu-text nav-menu-contact" href="#page-contact">CONTACT</a></li>
+                    <li><a className="nav-menu-text nav-menu-works" href="#p">MY WORK</a></li>
+                    <li><a className="nav-menu-text nav-menu-about" href="#">ABOUT</a></li>
+                    <li><a className="nav-menu-text nav-menu-contact" href="#">CONTACT</a></li>
                 </ul>
             </div>
         </div>
