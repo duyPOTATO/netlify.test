0cd9888256fba02e02f79fbe4f47f94df595c1b9
duPO <dupo.duypotato@gmail.com>
2025-07-10 22:38:10 +0700

    sửa tiếp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './styles/App.css';
 import './styles/global.css';
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
 import FNavigation from './features/final/components/fNavigation';
 function App() {
   return (
