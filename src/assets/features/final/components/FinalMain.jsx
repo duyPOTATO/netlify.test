3e27b97233be670a972c5a0be9dd7b2d5f54f485
DuyPC <dupo.duypotato@gmail.com>
2025-07-21 21:38:53 +0700

    sửa lại tên file chữ cái đầu viết thường

--- src/assets/features/final/components/FinalMain.jsx
+++ src/assets/features/final/components/FinalMain.jsx
@@ -1,4 +1,4 @@
-import EssayPreview from './EssayPreview';
+import EssayPreview from './essayPreview';
 import Hero from './hero';
 import FeedbackSection from './feedback';
 import Skills from './skills';
